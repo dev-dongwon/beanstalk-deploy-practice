365645c1173c3cab4264b6f70ffaa916644315b7
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-01 16:56:22 +0900

    Feat : flower api 추가

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("go and don't give up");
 });
+
+app.get("/flower", (req, res) => {
+  res.json({
+    name: "dongwon",
+    feeling: "angry"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
 });
