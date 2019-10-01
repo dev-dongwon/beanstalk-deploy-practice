7732f2f267ef829d6c0e7bd4123c5a46ef455df4
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-01 17:11:03 +0900

    Feat : react build 후 server에서 정적 파일 읽도록 함

---
+++ client/build/precache-manifest.4d6a5f58d14488211e7660d7f46bb9d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2198eee2d8dbd6ef98e9784df92cfdab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e3bad7683663bd3a738",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d92c54b8ba2e094657a0",
+    "url": "/static/js/2.711dc7ac.chunk.js"
+  },
+  {
+    "revision": "4e3bad7683663bd3a738",
+    "url": "/static/js/main.7f268efd.chunk.js"
+  },
+  {
+    "revision": "6ec9d595e5afe4912148",
+    "url": "/static/js/runtime-main.bdc68d36.js"
+  }
+]);
