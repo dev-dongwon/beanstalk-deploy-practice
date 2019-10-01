e5b8318ceadb06f2cd276efa61fe8a335ebff63d
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-01 17:11:32 +0900

    Feat : 서버에 빌드된 정적 파일 읽는 코드 추가

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+import path from "path";
 const express = require("express");
 const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
+
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("/", (req, res) => {
   res.send("go and don't give up");
